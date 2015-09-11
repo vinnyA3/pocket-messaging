168f51c29e3949c5c7ad108bc83274101ed88274
vinnyA3 <vinaceto@yahoo.com>
2015-09-11 02:24:06 -0400

    Database Connection File

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 	//connect to my sql
 	ini_set('display_errors',"1");
 	
-	$con = mysqli_connect("localhost", "root", "36steel9", "shoutIt");
+	$con = mysqli_connect("localhost", "<username>", "<password>", "shoutIt");
 	//Test connection
 	if(mysqli_connect_errno()) {
 		die("Failed to connect: ".mysqli_connect_error() );
